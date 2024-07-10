c52f5544a8ffee94aca0f93f1694d822631b3bfe
Ruslan Mukhamedvaleev <r1554899@gmail.com>
2024-07-09 21:34:11 -0700

    Fix banner overflow

--- components/sections/999 - banner.jsx
+++ components/sections/999 - banner.jsx
@@ -5,7 +5,7 @@
 
 export default function Banner() {
   return (
-    <div className="sticky z-20 top-0">
+    <div className="sticky z-20 top-0 -mb-8">
       <a href="https://www.foym.org/work"className="flex items-center gap-x-6 bg-black px-6 py-2.5 sm:px-3.5 sticky z-20 top-0">
         <div className="mx-auto">
           <p className="text-sm leading-6 text-white">
